2201b6226cc3d835ee52a040d8dfa38542f2edf9
Alexis <alexis.morin.mail@gmail.com>
2017-11-11 05:14:46 -0500

    build alexis 1
    quite obvious

---
+++ JN3.01/Assets/Art/Environment/UVScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroller : MonoBehaviour {
+
+ public float scrollSpeed;
+ Renderer rend;
+
+ 
+	void Start () {
+		rend = GetComponent<Renderer>();
+		//InvokeRepeating("Tile",0f,scrollSpeed);
+	}
+	
+
+	void Update () {
+		
+		rend.material.mainTextureOffset = new Vector2(0, -0.05f* Time.timeSinceLevelLoad);
+	}
+}
