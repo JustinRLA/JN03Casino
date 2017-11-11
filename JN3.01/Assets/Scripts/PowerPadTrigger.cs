7d5260e9f1ed6b4666de22ed4272d4db917adcf7
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2017-11-11 05:55:59 -0500

    Added second player

--- JN3.01/Assets/Scripts/PowerPadTrigger.cs
+++ JN3.01/Assets/Scripts/PowerPadTrigger.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerPadTrigger : MonoBehaviour {
-
-    public int platformCount = 0;
-
     
     public GameObject[] platforms;
 
