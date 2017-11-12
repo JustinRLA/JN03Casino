4d7b52ab60d17389c930334d6c709a8bd384c81d
Alexis <alexis.morin.mail@gmail.com>
2017-11-12 02:32:35 -0500

    Ending Cutscene

---
+++ JN3.01/Assets/Art/Characters/Animations/TimedPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPlay : MonoBehaviour {
+
+public float timer;
+
+	void Start () {
+
+Wait();
+	
+	}
+	
+	// Update is called once per frame
+ IEnumerator Wait() {
+			yield return new WaitForSeconds(timer);
+	GetComponent<Animation>().Play();
+	}
+}
