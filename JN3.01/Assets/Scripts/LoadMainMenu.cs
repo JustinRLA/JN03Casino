16a660fdc577292111eed108e757b0a12b6cec2c
Alexis <alexis.morin.mail@gmail.com>
2017-11-12 12:25:22 -0500

    Added Music and Menu Management

---
+++ JN3.01/Assets/Scripts/LoadMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour {
+
+    void MainMenu()
+    {
+        //Set menu to Main Menu Screen
+        ApplicationModel.menuState = 0;
+        SceneManager.LoadScene(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			MainMenu();
+		}
+	}
+}
